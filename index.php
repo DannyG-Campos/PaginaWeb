21779babaace42f54994e3b748949335c9e4a02b
DannyG-Campos <74079288+DannyG-Campos@users.noreply.github.com>
2021-05-29 19:01:02 -0400

    Update index.php
    Código que envió Carlos para la barra de navegación

--- index.php
+++ index.php
@@ -1,14 +1,87 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Pagina Web</title>
-	</head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Supermercado</title>
+</head>
+<body>
+ 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><i class="fa fa-fw fa-home"></i>Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Comestibles</a> 
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Bebidas</a> 
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"></i> Frutas y Vegetales</a> 
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Limpieza</a> 
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Ofertas</a> 
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"> Recetas</a> 
+            </li>
+           
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+          <a href="#" class="nav-link"><i class="fa fa-fw fa-user"></i> Login</a>
+        </div>
+      </nav>
 
-	<body>
-		<?php
-			echo "Hola mundo <br>";
-		?>
-	</body>
+   
 
+      <div class="container">
+        
+        <div class="card" style="width:200px">
+          <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+          <div class="card-body">
+            <h4 class="card-title">Aceites</h4>
+            <a href="#" class="btn btn-primary">IR</a>
+          </div>
+          <div class="card" style="width:200px">
+            <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">Aceites</h4>
+              <a href="#" class="btn btn-primary">IR</a>
+            </div>
+            <div class="card" style="width:200px">
+                <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+                <div class="card-body">
+                  <h4 class="card-title">Aceites</h4>
+                  <a href="#" class="btn btn-primary">IR</a>
+                </div>
+                <div class="card" style="width:200px">
+                    <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                      <h4 class="card-title">Aceites</h4>
+                      <a href="#" class="btn btn-primary">IR</a>
+                    </div>
+        </div>
+      
+    
+</body>
 </html>
