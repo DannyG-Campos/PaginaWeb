5f11eebec8bca16d953295e860c9711992956354
DannyG-Campos <74079288+DannyG-Campos@users.noreply.github.com>
2021-05-29 18:06:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-//Inicio
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Pagina Web</title>
+	</head>
+
+	<body>
+		<?php
+			echo "Hola mundo <br>";
+		?>
+	</body>
+
+</html>
