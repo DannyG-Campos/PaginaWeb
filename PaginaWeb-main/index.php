f2d3871856b34968c3d216129b0d133c5f62eab8
Eze0 <74229020+Eze0@users.noreply.github.com>
2021-06-16 02:36:40 -0400

    Add files via upload

---
+++ PaginaWeb-main/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Supermercado</title>
+</head>
+<body>
+ 
+   <!-- carrusel inicio --> 
+   <h1>Ofertas</h1>
+   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img1/Guineo.jpg" alt="Guineo">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>$4</h5>
+    <p>Antes $20</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img1/Platano.jpg" alt="Platano">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>$8</h5>
+    <p>Antes $10</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img1/Salami.jpg" alt="Salami">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>$250</h5>
+    <p>Antes $300</p>
+  </div>
+    </div>
+  <div class="carousel-item">
+      <img class="d-block w-100" src="img1/Yuca.jpg" alt="Yuca">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>Libra $20</h5>
+    <p>Antes $25</p>
+  </div>
+    </div>
+  <div class="carousel-item">
+      <img class="d-block w-100" src="img1/Aceite.jpg" alt="Aceite">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>$200</h5>
+    <p>Antes $225</p>
+  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+   <!-- carrusel fin -->
+
+   <div class="container">
+        
+        <div class="card" style="width:200px">
+          <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+          <div class="card-body">
+            <h4 class="card-title">Aceites</h4>
+            <a href="#" class="btn btn-primary">IR</a>
+          </div>
+          <div class="card" style="width:200px">
+            <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+            <div class="card-body">
+              <h4 class="card-title">Aceites</h4>
+              <a href="#" class="btn btn-primary">IR</a>
+            </div>
+            <div class="card" style="width:200px">
+                <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+                <div class="card-body">
+                  <h4 class="card-title">Aceites</h4>
+                  <a href="#" class="btn btn-primary">IR</a>
+                </div>
+                <div class="card" style="width:200px">
+                    <img class="card-img-top" src="aceites.jpg" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                      <h4 class="card-title">Aceites</h4>
+                      <a href="#" class="btn btn-primary">IR</a>
+                    </div>
+        </div>
+</body>
+</html>
